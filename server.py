b40c59ce18a4a79a76a71354d6b8403526fa56ed
Angelo Guerrero Medina <angelomedina@estudiantec.cr>
2019-09-19 22:42:42 -0600

    test

--- server.py
+++ server.py
@@ -1,29 +1,35 @@
 # tutorial
 # https://realpython.com/python-sockets/
-import socket
-import json
-host = '127.0.0.1'
-port = 5000
-print('Server listening: '+ host +' '+ str(port))
+import socket
+import json
+host = '127.0.0.1'
+port = 5000
+
+print('server listening: '+host+' '+str(port))
 # socket.AF_INET: means protocol IPv4
 # socket.SOCK_STREAM: used for TCP
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
-    s.listen()
-    conn, addr = s.accept()
-    #connection to download a file
-    print('connected by', addr)
-    data = conn.recv(1024)
-    #convert the response in string; then concat the path and the filename(name)
-    filename = str(repr(data))
-    name = filename[2:len(filename)-1]
-    filepath = 'server-file/' + name
-    f = open(filepath, 'rb')
-    value = f.read(1024)
-    while (value):
-        #conn.send(value)
-        print('sent file')
-        value = f.read(1024)
-    f.close()
-print('Done sending')
-conn.close() 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host,port))
+    s.listen()
+    conn, addr = s.accept()
+    #connection to download a file
+    print('connected by ',addr)
+    data = conn.recv(1024)
+    #convert the response in string; then concat the path and the filename(name)
+    filename = str(repr(data))
+    name = filename[2:len(filename)-1]
+    filepath = 'server-file/'+name
+    f = open(filepath, 'rb')
+    value = f.read(1024)
+
+    while (value):
+        conn.send(value)
+        print('sent file')
+        value = f.read(1024)
+    f.close()
+print('Done sending')
+conn.close()
+    
+    
+
+
